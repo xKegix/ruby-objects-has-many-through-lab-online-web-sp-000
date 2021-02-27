05b2022aa47dcc890295b26fe81aa27968ebe0d3
Keginold Philip <keginoldp@hotmail.com>
2021-02-27 13:34:41 +0000

    Done.

---
+++ lib/doctor.rb
@@ -0,0 +1,28 @@
+
+class Doctor
+  attr_accessor :name
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_appointment(date, patient)
+    Appointment.new(date, patient, self)
+  end
+
+  def appointments
+    Appointment.all.select {|app| app.doctor == self}
+  end
+
+  def patients
+    appointments.map {|appointment| appointment.patient}
+  end
+
+
+end
